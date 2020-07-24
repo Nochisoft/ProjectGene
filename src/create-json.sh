b503dab70a2822734ad02e9fc48c7bf574689462
Lalitha Nochili <lanochil@microsoft.com>
2020-07-24 15:11:59 +0530

    Create create-json.sh
    initial commit

---
+++ src/create-json.sh
@@ -0,0 +1,14 @@
+#Script to update the SPN file with azure spn credentials
+#uses the file passed as the parameter
+subscriptionId=`az account show --query id`
+tenantId=`az account show --query tenantId`
+temp=`cat $1`
+appId=`echo $temp | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["appId"]'`
+password=`echo $temp | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["password"]'`
+## Copy the template file and update it with the SPN credentials
+cp azure-access-template.json github-secret.json
+sed -i -e "s/<subscriptionId>/$subscriptionId/g" github-secret.json
+sed -i -e "s/<tenantId>/$tenantId/g" github-secret.json
+sed -i -e "s/<clientId>/$appId/g" github-secret.json
+sed -i -e "s/<password>/$password/g" github-secret.json
+
